b7d0ffaf4e72be4510deb9d5eeba292e88401277
Micheal Oriyomi <michealoriyomi01@gmail.com>
2023-02-17 05:33:07 +0100

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string of text in standrd output
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints a string of text on standard output
+ *
+ * Return: 0 on success, non-zero otherwise.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
